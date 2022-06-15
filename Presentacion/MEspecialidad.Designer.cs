7b14a1a5233dcb28a15a19f333a8907737759935
HubCJ <u20201b742@upc.edu.pe>
2022-06-15 11:01:36 -0500

    no hice nada

--- Presentacion/MEspecialidad.Designer.cs
+++ Presentacion/MEspecialidad.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MEspecialidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(656, 391);
+            this.Name = "MEspecialidad";
             this.Text = "MEspecialidad";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
